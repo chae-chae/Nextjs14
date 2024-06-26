c084fc51a77457b37bbf51dc9a418b9906193a35
Chaekyeong Jeong <chae-chae@Chaekyeongui-MacBookPro.local>
2024-06-26 14:07:52 +0900

    update movies/[id]/page.tsx using parallel requests

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,7 +1,27 @@
-export default function MovieDetail({
+import { API_URL } from "../../../(home)/page";
+
+async function getMovie(id: string) {
+  await new Promise((resolve) => setTimeout(resolve, 5000));
+  console.log(`Fetching movies: ${Date.now()}`);
+  const response = await fetch(`${API_URL}/${id}`);
+  return response.json();
+}
+
+async function getVideos(id: string) {
+  await new Promise((resolve) => setTimeout(resolve, 5000));
+  console.log(`Fetching videos: ${Date.now()}`);
+  const response = await fetch(`${API_URL}/${id}/videos`);
+  return response.json();
+}
+
+export default async function MovieDetail({
   params: { id },
 }: {
   params: { id: string };
 }) {
-  return <h1>Movie {id}</h1>;
+  console.log("--------");
+  console.log("start fetching");
+  const [movie, videos] = await Promise.all([getMovie(id), getVideos(id)]);
+  console.log("finish fetching");
+  return <h1>{movie.title}</h1>;
 }
