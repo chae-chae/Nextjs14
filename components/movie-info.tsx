9894deabe2800a779380483855cf787e1253b28a
Jeong Chae Kyeong <chaechae.d3v@gmail.com>
2024-06-27 15:24:39 +0900

    add generateMetadata function

--- components/movie-info.tsx
+++ components/movie-info.tsx
@@ -1,8 +1,7 @@
 import styles from "../styles/movie-info.module.css";
 import { API_URL } from "../app/(home)/page";
 
-async function getMovie(id: string) {
-  console.log(`Fetching movies: ${Date.now()}`);
+export async function getMovie(id: string) {
   const response = await fetch(`${API_URL}/${id}`);
   return response.json();
 }
